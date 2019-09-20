66b74b14de39fbc2a80988a7475d8fae13174ba7
Kai Jun Eer <s1800883@herrera.inf.ed.ac.uk>
2019-09-20 14:10:29 +0100

    initial commit

---
+++ src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -0,0 +1,21 @@
+package uk.ac.ed.inf.powergrab;
+
+public class Position {
+	
+	public double latitude;
+	public double longitude;
+	
+	public Position(double latitude, double longitude) {
+		
+	}
+	
+	/*
+	public Position nextPostion(Direction direction) {
+
+	}
+	
+	public boolean inPlayArea() {
+		
+	}
+    */ 
+}
