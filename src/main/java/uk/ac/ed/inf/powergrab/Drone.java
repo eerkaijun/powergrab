82ad8cc930264fb98132c9512939285867cdef90
Eer Kai Jun <kaijuneer@gmail.com>
2019-09-27 16:23:35 +0100

    constructed drone class

---
+++ src/main/java/uk/ac/ed/inf/powergrab/Drone.java
@@ -0,0 +1,13 @@
+package uk.ac.ed.inf.powergrab;
+
+public class Drone {
+	
+	public double coin;
+	public double power;
+	
+	public Drone(double coin, double power) {
+		this.coin = coin;
+		this.power = power;
+	}
+
+}
