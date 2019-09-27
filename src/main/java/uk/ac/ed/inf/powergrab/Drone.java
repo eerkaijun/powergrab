7be5619da619d916338914c043a1bd5ad5ce7930
Eer Kai Jun <kaijuneer@gmail.com>
2019-09-27 17:08:20 +0100

    update coin and power functions

--- src/main/java/uk/ac/ed/inf/powergrab/Drone.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Drone.java
@@ -4,10 +4,21 @@ public class Drone {
 	
 	public double coin;
 	public double power;
+	public Position position;
 	
-	public Drone(double coin, double power) {
+	public Drone(double coin, double power, double latitude, double longitude) {
 		this.coin = coin;
 		this.power = power;
+		this.position = new Position(latitude, longitude);
 	}
-
+    
+	public void updateCoin (double coinIn) {
+		this.coin = this.coin + coinIn;
+		if (this.coin < 0) this.coin = 0; 
+	}
+	
+	public void updatePower (double powerIn) {
+		this.power = this.power + powerIn;
+		if (this.power < 0) this.power = 0;
+	}
 }
