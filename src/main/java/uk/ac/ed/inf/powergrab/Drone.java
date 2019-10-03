fdb2d1e1e2ff1df4df3e0108522e7f149b5a10dc
Eer Kai Jun <kaijuneer@gmail.com>
2019-10-03 13:07:26 +0100

    update class hierarchy

--- src/main/java/uk/ac/ed/inf/powergrab/Drone.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Drone.java
@@ -1,15 +1,14 @@
 package uk.ac.ed.inf.powergrab;
 
-public class Drone {
+public class Drone extends Position {
 	
 	public double coin;
 	public double power;
-	public Position position;
 	
 	public Drone(double coin, double power, double latitude, double longitude) {
+		super(latitude, longitude);
 		this.coin = coin;
 		this.power = power;
-		this.position = new Position(latitude, longitude);
 	}
     
 	public void updateCoin (double coinIn) {
@@ -20,5 +19,11 @@ public class Drone {
 	public void updatePower (double powerIn) {
 		this.power = this.power + powerIn;
 		if (this.power < 0) this.power = 0;
+	}
+	
+	public double calculateDistance(double latitudeIn, double longitudeIn) {
+		double a = Math.pow((this.latitude - latitudeIn),2);
+		double b = Math.pow((this.longitude - longitudeIn),2);
+		return (Math.sqrt(a+b));
 	}
 }
