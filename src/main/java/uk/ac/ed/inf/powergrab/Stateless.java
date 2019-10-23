01b30ace288d0131da4ea00f17fcf6581470a79f
Eer Kai Jun <kaijuneer@gmail.com>
2019-10-23 17:32:19 +0100

    single move for the stateless drone

--- src/main/java/uk/ac/ed/inf/powergrab/Stateless.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Stateless.java
@@ -2,11 +2,10 @@ package uk.ac.ed.inf.powergrab;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.google.gson.JsonElement;
 import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.Geometry;
-import com.mapbox.geojson.Point;
 
 public class Stateless {
 	
@@ -19,28 +18,66 @@ public class Stateless {
 	}
 	
 	public void simulation(String url, double latitudeInitial, double longitudeInitial) {
-		//Initialise the map based on the given URL
-		Maps map = new Maps(url);
-		List<Feature> features = map.readMap();
-		List<Feature> features_local = new ArrayList<Feature>();
+			
+		//Initialise random seed
+		Random rnd = new Random(this.seed);
 		
 		//Initialise a stateless drone
 		Drone drone = new Drone(0.0, 250.0, latitudeInitial, longitudeInitial);
 		
+		//Initialise the map based on the given URL
+		Maps map = new Maps(url);
+		List<Feature> features = map.readMap();
+		
+		List<Feature> features_positive = new ArrayList<Feature>(); //List of features with positive coin within area of rough estimation
+		List<Feature> features_valid = new ArrayList<Feature>(); //List of valid features after move by drone
+
+		//Rough estimation on the positive charging stations within the next move of the drone
+		//The stations are stored in the variable features_positive
 		for (int i=0; i<50; i++) {
 			Feature f = features.get(i);
-			Geometry g = f.geometry();
-	        Point p = (Point) g;
-	        double longitudeIn = p.coordinates().get(0);
-	        double latitudeIn = p.coordinates().get(1);
-	        if(!drone.withinRange(latitudeIn, longitudeIn, 0.00055)){
-	        	features_local.add(f);
+			double[] coordinates = map.getCoordinates(f);
+	        if(drone.withinRange(coordinates[0], coordinates[1], 0.00055)){
+	        	JsonElement elm = f.getProperty("marker-symbol");
+	        	String symbol = elm.getAsString();
+	        	if(symbol == "lighthouse") {
+	        		features_positive.add(f);
+	        	}	       
 	        }
-	        //TODO: Minus power after every moves
 		}
 		
-		//update coin and power according to the nearest charging stations
+		//Initialise a test drone and choose a random direction to move
+		do {
+			Drone drone_test = drone;
+			int move = rnd.nextInt(16);
+			drone_test.nextPosition(Direction.compass.get(move));
+			for (int i=0; i<features_positive.size(); i++) {
+				Feature f = features_positive.get(i);
+				double[] coordinates = map.getCoordinates(f);
+				if(drone.withinRange(coordinates[0], coordinates[1], 0.00025)) {
+					features_valid.add(f);
+				}
+			}
 		
+			if(features_valid.size() > 0) {
+				drone.nextPosition(Direction.compass.get(move));
+				double[] distance = new double[features_valid.size()];
+				for (int i=0; i<features_valid.size(); i++) {
+					Feature f = features_valid.get(i);
+					double[] coordinates = map.getCoordinates(f);
+					distance[i] = Drone.calculateDistance(drone.latitude, drone.longitude, coordinates[0], coordinates[1]);
+				}
+				//Return the feature with the nearest distance
+				int index = Drone.minDist(distance);
+				//Update coin and power
+				Feature f = features_valid.get(index);
+				double coinIn = map.getCoin(f);
+				drone.updateCoin(coinIn);
+				double powerIn = map.getPower(f);
+				drone.updatePower(powerIn);
+				drone.updatePower(-1.25);
+			} 
+		} while(features_valid.size() == 0);
 	}
 	
 	
