fd2e0e37ab048724f523540eccbc85b16b55e3ba
Eer Kai Jun <kaijuneer@gmail.com>
2019-10-31 16:11:57 +0000

    moved functions to Distance class

---
+++ src/main/java/uk/ac/ed/inf/powergrab/Distance.java
@@ -0,0 +1,37 @@
+package uk.ac.ed.inf.powergrab;
+
+public class Distance {
+	
+	public static double calculateDistance(double latitude, double longitude, double latitudeIn, double longitudeIn) {
+		double x = Math.pow((longitude - longitudeIn),2);
+		double y = Math.pow((latitude - latitudeIn),2);
+		double distance = Math.sqrt(x+y);
+		return distance;
+	}
+	
+	//Return the minimum distance
+	public static double minDist(double[] array) {
+		double min = array[0];
+		for(int i=0; i<array.length; i++) {
+			if(array[i] < min) {
+				min = array[i];
+		    }
+		}
+		return min;
+	}
+	
+	
+	//Return the index of the minimum distance
+	public static int minIndex(double[] array) {
+		double min = array[0];
+		int index = 0;
+		for(int i=0; i<array.length; i++) {
+			if(array[i] < min) {
+				min = array[i];
+				index = i;
+		    }
+		}
+		return index;
+	}
+
+}
--- src/main/java/uk/ac/ed/inf/powergrab/Drone.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Drone.java
@@ -33,32 +33,4 @@ public class Drone extends Position {
 		if (this.power < 0) this.power = 0;
 	}
 	
-	public static double calculateDistance(double latitude, double longitude, double latitudeIn, double longitudeIn) {
-		double x = Math.pow((longitude - longitudeIn),2);
-		double y = Math.pow((latitude - latitudeIn),2);
-		double distance = Math.sqrt(x+y);
-		return distance;
-	}
-	
-	public static double minDist(double[] array) {
-		double min = array[0];
-		for(int i=0; i<array.length; i++) {
-			if(array[i] < min) {
-				min = array[i];
-		    }
-		}
-		return min;
-	}
-	
-	public static int minIndex(double[] array) {
-		double min = array[0];
-		int index = 0;
-		for(int i=0; i<array.length; i++) {
-			if(array[i] < min) {
-				min = array[i];
-				index = i;
-		    }
-		}
-		return index;
-	}
 }
