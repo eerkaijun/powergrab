f4fafff0bb310d407a40eedf4cb217d26ad88a9b
Eer Kai Jun <kaijuneer@gmail.com>
2019-11-23 15:55:16 +0000

    strategy design pattern initial restructuring

--- src/main/java/uk/ac/ed/inf/powergrab/Stateless.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Stateless.java
@@ -1,162 +1,115 @@
 package uk.ac.ed.inf.powergrab;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
 
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 
-public class Stateless {
+public abstract class Stateless implements Strategy{
 	
-	public int moves;
-	public int seed;
+	protected Drone drone;
+	protected List<Station> stations;
+	protected HashMap<Integer, Station> positive; 
+	protected HashMap<Integer, Station> neutral; 
+	protected HashMap<Integer, Station> negative; 
+	protected List<Integer> invalid_directions;
 	
-	public Stateless(int seed) {
-		this.moves = 250;
-		this.seed = seed;
-	}
-	
-	public void simulation(String url, String filename, double latitudeInitial, double longitudeInitial) {
-			
-		//Initialise random seed
-		Random rnd = new Random(this.seed);
-		
-		//Initialise a stateless drone
-		Drone drone = new Drone(0.0, 250.0, latitudeInitial, longitudeInitial);
-		System.out.println(drone.latitude);
-		System.out.println(drone.longitude);
-		
-		//Initialise the map based on the given URL
+	public Stateless(double latitudeInitial, double longitudeInitial, String url) {
+		this.drone = new Drone(0.0, 250.0, 250, latitudeInitial, longitudeInitial);
+		initialiseStations(url);
+		/*
 		Maps map = new Maps(url);
 		List<Feature> features = map.readMap();
+		this.stations = new ArrayList<Station>();
+		for (int i=0; i<50; i++) {
+			Feature f = features.get(i);
+			Station s = new Station();
+			s.getInfo(f);
+			this.stations.add(s);
+		}*/
+		
+		this.positive = new HashMap<Integer, Station>(); 
+		this.neutral = new HashMap<Integer, Station>(); 
+		this.negative = new HashMap<Integer, Station>(); 
+		this.invalid_directions = new ArrayList<Integer>();
+	}
+	
+	public void simulation(String filename, String url, int seed) {
+			
+		//Initialise random seed
+		Random rnd = new Random(seed);
+		
+		//Initialise a stateless drone
+		//Drone drone = new Drone(0.0, 250.0, latitudeInitial, longitudeInitial);
+		//System.out.println(drone.latitude);
+		//System.out.println(drone.longitude);
+		
+		//Initialise the map based on the given URL
+		//Maps map = new Maps(url);
+		//List<Feature> features = map.readMap();
 		
 		//Initialise a list of charging stations on the map
-		List<Station> stations = new ArrayList<Station>();
+		//List<Station> stations = new ArrayList<Station>();
 		
 		//Initialise a list of Point indicating the drone's flight path
 		List<Point> points = new ArrayList<Point>();
-		Point p0 = Point.fromLngLat(longitudeInitial, latitudeInitial);
+		Point p0 = Point.fromLngLat(this.drone.longitude, this.drone.latitude);
 		points.add(p0);
 		
 		//Convert features into station instances
+		/*
 		for (int i=0; i<50; i++) {
 			Feature f = features.get(i);
 			Station s = new Station();
 			s.getInfo(f);
 			stations.add(s);
-		}
+		}*/
 		
-		while(this.moves>0 && drone.power>0) {
+		while(this.drone.moves>0 && this.drone.power>0) {
 			
 			//Drone coordinates in String before move
-			String pre_latitude = Double.toString(drone.latitude);
-			String pre_longitude = Double.toString(drone.longitude);
+			String pre_latitude = Double.toString(this.drone.latitude);
+			String pre_longitude = Double.toString(this.drone.longitude);
 			String direction;
 			
 			//Initialise HashMap -- Integer for directions and Station for nearest charging station in that particular direction
-			HashMap<Integer, Station> positive = new HashMap<Integer, Station>(); 
-			HashMap<Integer, Station> neutral = new HashMap<Integer, Station>(); 
-			HashMap<Integer, Station> negative = new HashMap<Integer, Station>(); 
-			List<Integer> invalid_directions = new ArrayList<Integer>();
+			//HashMap<Integer, Station> positive = new HashMap<Integer, Station>(); 
+			//HashMap<Integer, Station> neutral = new HashMap<Integer, Station>(); 
+			//HashMap<Integer, Station> negative = new HashMap<Integer, Station>(); 
+			//List<Integer> invalid_directions = new ArrayList<Integer>();
 			
-			for (int i=0; i<16; i++) {
-				System.out.println("The drone is now in direction " + i);
-				Drone drone_test = drone;
-				drone_test = drone_test.nextPosition(Direction.compass.get(i));
-				if (drone_test.inPlayArea()) {
-					double[] distance = new double[50];
-					for (int j=0; j<50; j++) {
-						Station s = stations.get(j);
-						distance[j] = Distance.calculateDistance(drone_test.latitude, drone_test.longitude, s.coordinates[0], s.coordinates[1]);
-					}
-					System.out.println("The distance size is " + distance.length);
-					if (Distance.minDist(distance) <= 0.00025) {
-						int index = Distance.minIndex(distance);
-						System.out.println("The charging station with the minimum distance is " + index);
-						Station s = stations.get(index);
-						if (s.coins>0 || s.power>0) positive.put(i, s);
-						else if (s.coins<0 || s.power<0) negative.put(i, s);
-						else neutral.put(i, s);
-					} 
-				} else {
-					System.out.println("Drone_test in the direction of " + i + " is out of playing area.");
-					invalid_directions.add(i);
-				}
-			}
+			this.positive.clear();
+			this.neutral.clear();
+			this.negative.clear();
+			this.invalid_directions.clear();
+			
+			checkNextPossibleMove();
 			
 			System.out.println("Positive hashmap " + positive);
 			System.out.println("Neutral hashmap " + neutral);
 			System.out.println("Negative hashmap " + negative);
 			
-			if (positive.size() > 0) {
-				Set<Integer> key_set = positive.keySet();
-				Integer[] keys = key_set.toArray(new Integer[0]);
-				int select = rnd.nextInt(keys.length);
-				int move = keys[select];
-				drone = drone.nextPosition(Direction.compass.get(move));
-				direction = Direction.directions_str[move]; 
-				Station s = positive.get(move);
-				drone.updateCoin(s.coins);
-				drone.updatePower(s.power);
-				s.coins = 0;
-				s.power = 0;
-				for (int i=0; i<50; i++) {
-					if (stations.get(i).id.equals(s.id)) {
-						stations.set(i, s);
-						break;
-					}
-				}
-			} else if (negative.size() > 0 && negative.size() < (16-invalid_directions.size())) {
-				Set<Integer> key_set = negative.keySet();
-				List<Integer> key_list = new ArrayList<Integer>(key_set);
-				Integer[] directions = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-				List<Integer> valid = new ArrayList<Integer>(Arrays.asList(directions));
-				valid.removeAll(key_list);
-				valid.removeAll(invalid_directions);
-				int select = rnd.nextInt(valid.size());
-				int move = valid.get(select);
-				drone = drone.nextPosition(Direction.compass.get(move));
-				direction = Direction.directions_str[move]; 
-			} else if (negative.size() == (16-invalid_directions.size())) {
-				Set<Integer> key_set = negative.keySet();
-				Integer[] keys = key_set.toArray(new Integer[0]);
-				int select = rnd.nextInt(keys.length);
-				int move = keys[select];
-				drone = drone.nextPosition(Direction.compass.get(move));
-				direction = Direction.directions_str[move]; 
-				Station s = negative.get(move);
-				drone.updateCoin(s.coins);
-				drone.updatePower(s.power);
-				s.coins = 0;
-				s.power = 0;
-				for (int i=0; i<50; i++) {
-					if (stations.get(i).id.equals(s.id)) {
-						stations.set(i, s);
-						break;
-					}
-				}
+			if (this.positive.size() > 0) {
+				direction = moveTowardsPositive(rnd);
+			} else if (this.negative.size() > 0 && this.negative.size() < (16-this.invalid_directions.size())) {
+				direction = moveAwayFromNegative(rnd);
+			} else if (this.negative.size() == (16-this.invalid_directions.size())) {
+				direction = moveLeastNegative(rnd);
 			} else {
-				Integer[] directions = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-				List<Integer> valid = new ArrayList<Integer>(Arrays.asList(directions));
-				valid.removeAll(invalid_directions);
-				int select = rnd.nextInt(valid.size());
-				int move = valid.get(select);
-				drone = drone.nextPosition(Direction.compass.get(move));
-				direction = Direction.directions_str[move]; 
+				direction = moveRandomly(rnd);
 			}
 			
-			drone.updatePower(-1.25);
-			this.moves--;
-			System.out.println("Total moves left: " + this.moves);
-			System.out.println("Coin values after " + this.moves + " is: " + drone.coin);
-			System.out.println("Power values after " + this.moves + " is: " + drone.power);
-			System.out.println("Latitude values after " + this.moves + " is: " + drone.latitude);
-			System.out.println("Longitude values after " + this.moves + " is: " + drone.longitude);
+			this.drone.updatePower(-1.25);
+			this.drone.moves = this.drone.moves - 1;
+			System.out.println("Total moves left: " + this.drone.moves);
+			System.out.println("Coin values after " + this.drone.moves + " is: " + this.drone.coin);
+			System.out.println("Power values after " + this.drone.moves + " is: " + this.drone.power);
+			System.out.println("Latitude values after " + this.drone.moves + " is: " + this.drone.latitude);
+			System.out.println("Longitude values after " + this.drone.moves + " is: " + this.drone.longitude);
 			System.out.println(" ");
 			
 			//Drone values after move
@@ -173,12 +126,39 @@ public class Stateless {
 			
 		}
 		
+		Maps map = new Maps(url);
+		List<Feature> features = map.readMap();
 		FeatureCollection fc = map.writeMap(points, features);
-		File.writeGeoJSONFile("testing.geojson", fc);
+		File.writeGeoJSONFile("test.geojson", fc);
 		
 		for (int i=0; i<50; i++) {
-			System.out.println(i + " station has coin value of " + stations.get(i).coins);
+			System.out.println(i + " station has coin value of " + this.stations.get(i).coins);
 		}
 		
-	}	
+	}
+	
+	private void initialiseStations(String url) {
+		
+		Maps map = new Maps(url);
+		List<Feature> features = map.readMap();
+		this.stations = new ArrayList<Station>();
+		for (int i=0; i<50; i++) {
+			Feature f = features.get(i);
+			Station s = new Station();
+			s.getInfo(f);
+			this.stations.add(s);
+		}
+		
+	}
+	
+	abstract void checkNextPossibleMove();
+	
+	abstract String moveTowardsPositive(Random rnd);
+	
+	abstract String moveAwayFromNegative(Random rnd);
+	
+	abstract String moveLeastNegative(Random rnd);
+	
+	abstract String moveRandomly(Random rnd);
+	
 }
---
+++ src/main/java/uk/ac/ed/inf/powergrab/StatelessSim.java
@@ -0,0 +1,120 @@
+package uk.ac.ed.inf.powergrab;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class StatelessSim extends Stateless{
+	
+	public StatelessSim(double latitude, double longitude, String url) {
+		super(latitude, longitude, url);
+	}
+	
+	protected void checkNextPossibleMove() {
+		
+		for (int i=0; i<16; i++) {
+			System.out.println("The drone is now in direction " + i);
+			Drone drone_test = this.drone;
+			drone_test = drone_test.nextPosition(Direction.compass.get(i));
+			if (drone_test.inPlayArea()) {
+				double[] distance = new double[50];
+				for (int j=0; j<50; j++) {
+					Station s = this.stations.get(j);
+					distance[j] = Distance.calculateDistance(drone_test.latitude, drone_test.longitude, s.coordinates[0], s.coordinates[1]);
+				}
+				System.out.println("The distance size is " + distance.length);
+				if (Distance.minDist(distance) <= 0.00025) {
+					int index = Distance.minIndex(distance);
+					System.out.println("The charging station with the minimum distance is " + index);
+					Station s = this.stations.get(index);
+					if (s.coins>0 || s.power>0) this.positive.put(i, s);
+					else if (s.coins<0 || s.power<0) this.negative.put(i, s);
+					else this.neutral.put(i, s);
+				} 
+			} else {
+				System.out.println("Drone_test in the direction of " + i + " is out of playing area.");
+				this.invalid_directions.add(i);
+			}
+		}
+		
+	}
+	
+	protected String moveTowardsPositive(Random rnd) {
+		
+		Set<Integer> key_set = this.positive.keySet();
+		Integer[] keys = key_set.toArray(new Integer[0]);
+		int select = rnd.nextInt(keys.length);
+		int move = keys[select];
+		this.drone = this.drone.nextPosition(Direction.compass.get(move));
+		String direction = Direction.directions_str[move]; 
+		Station s = this.positive.get(move);
+		this.drone.updateCoin(s.coins);
+		this.drone.updatePower(s.power);
+		s.coins = 0;
+		s.power = 0;
+		for (int i=0; i<50; i++) {
+			if (this.stations.get(i).id.equals(s.id)) {
+				this.stations.set(i, s);
+				break;
+			}
+		}
+		return direction;
+	}
+	
+	protected String moveAwayFromNegative(Random rnd) {
+		
+		Set<Integer> key_set = this.negative.keySet();
+		List<Integer> key_list = new ArrayList<Integer>(key_set);
+		Integer[] directions = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+		List<Integer> valid = new ArrayList<Integer>(Arrays.asList(directions));
+		valid.removeAll(key_list);
+		valid.removeAll(invalid_directions);
+		int select = rnd.nextInt(valid.size());
+		int move = valid.get(select);
+		this.drone = this.drone.nextPosition(Direction.compass.get(move));
+		String direction = Direction.directions_str[move]; 
+		return direction;
+		
+	}
+	
+	protected String moveLeastNegative(Random rnd) {
+		
+		Set<Integer> key_set = this.negative.keySet();
+		Integer[] keys = key_set.toArray(new Integer[0]);
+		int select = rnd.nextInt(keys.length);
+		int move = keys[select];
+		this.drone = this.drone.nextPosition(Direction.compass.get(move));
+		String direction = Direction.directions_str[move]; 
+		Station s = this.negative.get(move);
+		this.drone.updateCoin(s.coins);
+		this.drone.updatePower(s.power);
+		s.coins = 0;
+		s.power = 0;
+		for (int i=0; i<50; i++) {
+			if (this.stations.get(i).id.equals(s.id)) {
+				this.stations.set(i, s);
+				break;
+			}
+		}
+		return direction;
+		
+	}
+	
+	protected String moveRandomly(Random rnd) {
+		
+		Integer[] directions = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+		List<Integer> valid = new ArrayList<Integer>(Arrays.asList(directions));
+		valid.removeAll(this.invalid_directions);
+		int select = rnd.nextInt(valid.size());
+		int move = valid.get(select);
+		this.drone = this.drone.nextPosition(Direction.compass.get(move));
+		String direction = Direction.directions_str[move]; 
+		return direction;
+		
+	}
+
+}
+
