f273093089afca8f26607ddd2336df3ef2f94fd0
Eer Kai Jun <kaijuneer@gmail.com>
2019-11-27 16:42:19 +0000

    commented code

--- src/main/java/uk/ac/ed/inf/powergrab/Strategy.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Strategy.java
@@ -2,6 +2,7 @@ package uk.ac.ed.inf.powergrab;
 
 public interface Strategy {
 	
+	//Declare simulation method that must be implemented by subclasses
 	void simulation(String filename, String url, int seed);
 
 }
