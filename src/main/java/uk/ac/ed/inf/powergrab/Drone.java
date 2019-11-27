d51f990ab0881342629d82032ccade266fae1292
Eer Kai Jun <kaijuneer@gmail.com>
2019-11-27 16:20:11 +0000

    added moves field

--- src/main/java/uk/ac/ed/inf/powergrab/Drone.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Drone.java
@@ -14,14 +14,12 @@ public class Drone extends Position {
 	}
 	
 	public Drone nextPosition(Direction direction) {
-		
 		double x_move, y_move;
 		x_move = 0.0003 * Math.sin(direction.angle);
 		y_move = 0.0003 * Math.cos(direction.angle);
 		Drone temp = new Drone(coin, power, moves, latitude, longitude);
 		temp.longitude = temp.longitude + x_move;
 		temp.latitude = temp.latitude + y_move;
-		
 		return temp;
 	}
     
